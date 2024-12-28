ab3e014a2968dafb685f17f7899b2b18d9a381d6
HARSH KUMAR <142934569+Harsh-Kumar-2003@users.noreply.github.com>
2024-12-28 21:57:29 +0530

    Update automate.js

--- automate.js
+++ automate.js
@@ -9,8 +9,7 @@ async function submit() {
         console.log("Response status:", response.status);
         if (response.ok) {
             console.log("Form submitted successfully!");
-            const redirectUrl = "https://ck.hdm3.in/lp.php?_save_form=true&hid=fb063bd6z6fd6je1ec858c16nf23c3vb075bf33&sid=085aaaf6&lead_data%5Bname%5D=harsh+Kumar&lead_data%5Bemail%5D=harshbkp123%40gmail.com&lead_data%5Bcontact_no%5D=7209985453"; // Replace with the actual URL
-            console.log("Redirecting to:", redirectUrl);
+            const redirectUrl = "https://ck.hdm3.in/lp.php?_save_form=true&hid=fb063bd6z6fd6je1ec858c16nf23c3vb075bf33&sid=085aaaf6&lead_data%5Bname%5D=harsh+Kumar&lead_data%5Bemail%5D=harshbkp123%40gmail.com&lead_data%5Bcontact_no%5D=7209985453";          console.log("Redirecting to:", redirectUrl);
             window.location.href = redirectUrl;
         } else {
             console.log("Form not submitted, status:", response.status);
